11a6e265702830f225a1bbf2220cf341ea51fa2c
pututwibowo <putut.info@gmail.com>
2024-03-12 14:39:24 +0700

    first commit

---
+++ resources/views/theme1/index.blade.php
@@ -0,0 +1,8 @@
+@extends('theme1.layout')
+
+@section('content')
+    <div class=" h-screen bg-cyan-500"></div>
+    <div class=" h-96 bg-red-500"></div>
+    <div class=" h-96 bg-green-500"></div>
+    <div class=" h-96 bg-yellow-500"></div>
+@endsection
